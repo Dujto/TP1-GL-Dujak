4772b837de01cf49550948fbe0801c6814a1bc09
Dujak <dujakthomas@gmail.com>
2024-03-18 10:34:05 +0100

    commit 3 pour le fichier de code java modifié et commenté

--- System_hospitalier.java
+++ System_hospitalier.java
@@ -3,29 +3,29 @@ package tp_git;
 import java.util.ArrayList;
 import java.util.List;
 
-public class System_hospitalier {
-
+public class System_hospitalier {     // classe qui modelise notre systeme 
+  
     private List<Requête> listRequetes;
 
-    public System_hospitalier(List<Requête> listRequetes) {
-        super();
+    public System_hospitalier(List<Requête> listRequetes) { // constructeur de la classe 
+        super();     
         this.listRequetes = listRequetes;
     }
 
-    public System_hospitalier() {
+    public System_hospitalier() {   methode pour creer un systeme de l'hopital 
         super();
         listRequetes = new ArrayList<>();
     }
 
-    public List<Requête> getListRequetes() {
+    public List<Requête> getListRequetes() { //retourne la liste de toutes les requetes faites
         return listRequetes;
     }
 
-    public void setListRequetes(List<Requête> listRequetes) {
+    public void setListRequetes(List<Requête> listRequetes) {  // methode qui fait une liste des requetes 
         this.listRequetes = listRequetes;
     }
 
-    public void recevoirRequete(Requête requete) {
+    public void recevoirRequete(Requête requete) {  //methode qui permet de recevoir une requete et faire tout les traitements nécessaires 
         listRequetes.add(requete);
         String analyse =  analyserRequete(requete);
         String comparaisonDonnees = comparerDonnes();
@@ -33,12 +33,12 @@ public class System_hospitalier {
         System.out.println(comparaisonDonnees);
     }
 
-    private String analyserRequete(Requête requête) {
+    private String analyserRequete(Requête requête) {    // methode pour analyser les requetes
         
         return "requête analysée";
     }
 
-    private String comparerDonnes() {
+    private String comparerDonnes() {    // methode qui compare les données 
         
         return "Cluster trouvé";
     }
